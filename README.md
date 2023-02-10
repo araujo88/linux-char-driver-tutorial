8ed58045a08815e4439fa3a6191a3b20cd3224f1
Leonardo Araujo <leonardo.aa88@gmail.com>
2023-02-10 10:33:13 -0300

    file operations

--- README.md
+++ README.md
@@ -2,10 +2,21 @@
 
 Implementation of a SCULL driver (Simple Character Utility for Loading Localities) example of Linux kernel development.
 
+## Linux Kernel version
+
+5.10.0-21-amd64
+
 ## Description
 
 SCULL is an region is memory that the kernel treats as if it was device or memory in device. It is used for learning to write device drivers and is also used for testing simple drivers.
 
 ### Datatypes
 
-- `dev_t` type (defined in <linux/types.h>) is used to hold device numbers - both the major and minor parts.
+- `dev_t` (defined in <linux/types.h>) is used to hold device numbers - both the major and minor parts
+- `const struct file_operations` (defined in <linux/fs.h>) hold the file operations performed on this device
+
+### Commands
+
+ - `cat /proc/devices` <br>
+ - `sudo tail -f /var/log/syslog` <br>
+ - `sudo mknod -m 666 /dev/scull c <MAJOR_NUMBER> <MINOR_NUMBER>` <br>
--- scull.c
+++ scull.c
@@ -6,49 +6,88 @@
 #include <linux/uaccess.h>
 #include <linux/slab.h>
 
-#define SCULL_MAJOR 0
-#define SCULL_MINOR 0
 #define SCULL_NR_DEVS 4
-#define SCULL_QUANTUM 4000
-#define SCULL_QSET 1000
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("SCULL - Simple Character Utility for Loading Localities");
 
+int scull_open (struct inode *pinode, struct file *pfile)
+{
+	printk(KERN_INFO "%s called\n", __FUNCTION__);
+	return 0;
+}
+
+
+ssize_t scull_read (struct file *pfile, char __user *buffer, size_t length, loff_t *offset)
+{
+	printk(KERN_INFO "%s called\n", __FUNCTION__);
+	return 0;
+}
+
+
+ssize_t scull_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset)
+{
+	printk(KERN_INFO "%s called\n", __FUNCTION__);
+	return length;
+}
+
+
+int scull_close(struct inode *pinode, struct file *pfile)
+{
+	printk(KERN_INFO "%s called\n", __FUNCTION__);
+	return 0;
+}
+
+
+static const struct file_operations scull_fops = {
+	.owner = THIS_MODULE,
+	.open = scull_open,
+	.read = scull_read,
+	.write = scull_write,
+	.release = scull_close
+};
+
+struct cdev scull_cdev;
+
 dev_t *dev;
+
 
 static int __init scull_init(void)
 {
 	int result;
 
-	printk(KERN_INFO "Scull init\n");
+	printk(KERN_INFO "%s called\n", __FUNCTION__);
 
+	// allocate kernel memory
 	dev = (dev_t*)kmalloc(sizeof(dev_t), GFP_KERNEL);
 
-	//dev = MKDEV(SCULL_MAJOR, SCULL_MINOR); // device numbers
-
-	// register_chr_dev_region obtaines one or more device numbers
-	// to work with.
-	// result = register_chrdev_region(*dev, SCULL_NR_DEVS, "scull");
+	if (dev == NULL) {
+		return 1;
+	}
 
 	// allocate a major number
-	result = alloc_chrdev_region(dev, SCULL_MINOR, SCULL_NR_DEVS, "scull");
-
-	printk(KERN_INFO "alloc_chrdev_region: %d\n", result);
-	printk(KERN_INFO "dev: %u\n", dev);
-	printk(KERN_INFO "Major number: %d | Minor number: %d\n", MAJOR(*dev), MINOR(*dev));
-
+	result = alloc_chrdev_region(dev, 0, SCULL_NR_DEVS, "scull");
+	
 	if (result < 0)
 		return result;
+
+	// register char dev
+	register_chrdev(MAJOR(*dev), "scull", &scull_fops);
+
+	cdev_init(&scull_cdev, &scull_fops);
 
 	return 0;
 }
 
 static void __exit scull_exit(void)
 {
-	printk(KERN_INFO "Scull exit\n");
+	printk(KERN_INFO "%s called\n", __FUNCTION__);
+	
+	cdev_del(&scull_cdev);
 
-	unregister_chrdev_region(MKDEV(SCULL_MAJOR, SCULL_MINOR), SCULL_NR_DEVS);
+	unregister_chrdev_region(MKDEV(MAJOR(*dev), 0), SCULL_NR_DEVS);
+	
+	unregister_chrdev(MAJOR(*dev), "scull");
 
 	kfree(dev);
 }
